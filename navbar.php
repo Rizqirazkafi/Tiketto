d714022a91302d56eeefde67f18900f71f36f81f
Rizqirazkafi <rizqirazkafi56@gmail.com>
2022-11-26 20:08:28 +0700

    Move navbar to single php file

---
+++ navbar.php
@@ -0,0 +1,25 @@
+<section id="nav">
+      <div class="container">
+        <header
+          class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4"
+        >
+          <h1
+            class="d-flex align-items-center  col-md-3 mb-2 mb-md-0 "
+          >
+          <a href="index.php" class="text-light text-decoration-none">Tiketto</a>
+          </h1>
+          <div class="col-md-3 text-end">
+            <a href="login.php"
+              ><button class="btn me-2 text-light" type="button">
+                Log In
+              </button></a
+            >
+            <a href="signup.php">
+              <button class="btn btnself-orange text-light" type="button">
+                Sign Up
+              </button>
+            </a>
+          </div>
+        </header>
+      </div>
+    </section>
